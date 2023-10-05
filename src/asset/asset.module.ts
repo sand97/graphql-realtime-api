767d761778f8bb7af699b409fde7f123e2895669
Bruce Guenkam <112434829+cedrick-naa@users.noreply.github.com>
2023-10-06 01:58:35 +0200

    Secure category endpoint

--- src/asset/asset.module.ts
+++ src/asset/asset.module.ts
@@ -1,23 +1,22 @@
-import {Module} from "@nestjs/common";
-import {AssetController} from "./asset.controller";
-import {AssetService} from "./asset.service";
-import {PrismaService} from "../prisma.service";
-import {MinioClientModule} from "../minio-client/minio-client.module";
-import {BullModule} from "@nestjs/bull";
-import {AssetProcessor} from "./asset.processor";
-import {ConfigService} from "@nestjs/config";
-import {AssetUrlService} from "./asset.url.service";
+import { Module } from '@nestjs/common';
+import { AssetController } from './asset.controller';
+import { AssetService } from './asset.service';
+import { MinioClientModule } from '../minio-client/minio-client.module';
+import { BullModule } from '@nestjs/bull';
+import { ConfigService } from '@nestjs/config';
+import { AssetUrlService } from './asset.url.service';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
-    imports: [
-        BullModule.registerQueue({
-            name: 'asset',
-        }),
-        MinioClientModule
-    ],
-    exports: [AssetUrlService],
-    controllers: [AssetController],
-    providers: [AssetUrlService, ConfigService, AssetProcessor, AssetService, PrismaService]
+  imports: [
+    BullModule.registerQueue({
+      name: 'asset',
+    }),
+    MinioClientModule,
+    PrismaModule,
+  ],
+  exports: [AssetUrlService],
+  controllers: [AssetController],
+  providers: [AssetUrlService, ConfigService, AssetService],
 })
-export class AssetModule {
-}
+export class AssetModule {}
